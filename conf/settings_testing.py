81ba6d41e8b776f719b352eb13767861e4b55908
smilelhong <1018199160@qq.com>
2018-01-09 13:33:59 +0800

    lhong

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -20,7 +20,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': '',                            # 你的数据库user
+        'USER': 'root',                            # 你的数据库user
         'PASSWORD': '',                        # 你的数据库password
         'HOST': '',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
